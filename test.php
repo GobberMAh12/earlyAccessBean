cdf8b04eaae13ccfe999f71aead59590529b9e9c
GobberMAh12 <danny.pham@elev.ga.lbs.se>
2026-04-24 10:06:18 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,13 @@
 <?php
-phpinfo();
+
 ?>
 
+<form method="POST">
+    Titel: <input type="text" name="titel" required><br><br>
+    Beskrivning:<br>
+    <textarea name="beskrivning" rows="5" cols="40"></textarea><br><br>
+    <input type="submit" value="Skicka">
+</form>
 
 <!DOCTYPE html>
 <html lang="en">
