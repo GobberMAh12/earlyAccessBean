dea6ee7e9db22808cd569285d2596c82faf8e0d3
GobberMAh12 <danny.pham@elev.ga.lbs.se>
2026-06-04 16:00:01 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,11 @@
 <?php
 $conn = new mysqli("localhost", "Deeznut", "A_b*512HEJ@", "turd");
 
-
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $sql = "INSERT INTO valfritt_namn (name) VALUES ('$username')";
+    $conn->query($sql);
+}
 ?>
 
 <!DOCTYPE html>
